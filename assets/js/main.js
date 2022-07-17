93885b759b95ed8821996f6d9f6134f3062be9d4
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-07-17 21:05:11 +0300

    select elements

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let data = document.getElementById('data');
+const val1 = document.getElementById("shedA");
+const val2 = document.getElementById("shedB");
+const val3 = document.getElementById("shedC");
+const val4 = document.getElementById("shedD");
+data.addEventListener('submit',dailystats);
+
