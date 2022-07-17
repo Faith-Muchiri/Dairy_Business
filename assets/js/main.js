10813dc910078523e6241e0a0f389636fbc4c504
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-07-17 21:07:32 +0300

    add function to calculate weekly produce

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,42 @@ data.addEventListener('submit',dailystats);
              document.getElementById('allsheds').innerHTML=" Totals ";
              document.getElementById('totalsheds').innerHTML=res;
     }
+
+    data.addEventListener('submit',weeklyStats);
+
+    function weeklyStats(){
+        let units="  litres."
+        let shedA= parseFloat(val1.value)*7;
+             //display to html page
+        document.getElementById('weeklyheading').innerHTML="WEEKLY STATS";        
+        document.getElementById('description1').innerHTML="Shed A weekly ";        
+        document.getElementById('value1').innerHTML=shedA;
+        document.getElementById('units1').innerHTML=units;
+            //display to console
+        console.log("WEEKLY STATS");
+        console.log("Shed A weekly collection : "+ shedA+ " Litres.");
+        console.log("  ");
+        let shedB= parseFloat(val2.value)*7;
+        document.getElementById('description2').innerHTML="Shed B weekly ";        
+        document.getElementById('value2').innerHTML=shedB;
+        document.getElementById('units2').innerHTML=units;
+        console.log("Shed B weekly collection : "+ shedB+ " Litres.");
+        console.log("  ");
+        let shedC= parseFloat(val3.value)*7;
+        document.getElementById('description3').innerHTML="Shed C weekly ";        
+        document.getElementById('value3').innerHTML=shedC;
+        document.getElementById('units3').innerHTML=units;
+        console.log("Shed C weekly collection : "+ shedC+ " Litres.");
+        console.log("  ");
+        let shedD= parseFloat(val4.value)*7;
+        document.getElementById('description4').innerHTML="Shed A weekly ";        
+        document.getElementById('value4').innerHTML=shedD;
+        document.getElementById('units4').innerHTML=units;
+        console.log("Shed D weekly collection : "+ shedD+ " Litres.");
+        console.log("  ");
+        console.log(" Totals from All Sheds");
+        
+        let totLitres = shedA+shedB+shedC+shedD;
+        document.getElementById('wtotals').innerHTML=totLitres+" Litres."
+        console.log(totLitres+" Ltr.");
+    }
